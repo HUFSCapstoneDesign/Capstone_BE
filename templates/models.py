ce7f3fe6628b821eff3446c56438400ce039ba0b
choidongkuen <danaver12@daum.net>
2023-05-08 14:42:03 +0900

    Feat : django model 구현
    Question, Answer 구현

--- templates/models.py
+++ templates/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
+
 # Create your models here.
+class Question(models.Model):
+    subject = models.CharField(max_length=200)
+    content = models.TextField()
+    create_date = models.DateTimeField()
+
+
+class Answer(models.Model):
+    # Question 삭제시 Answer 삭제
+    # Question 1 : N Answer
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    content = models.TextField()
+    create_date = models.DateTimeField()
