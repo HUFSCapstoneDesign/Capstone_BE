045d2cf4396e25ab1d9dc471e5d23cae03eef564
choidongkuen <danaver12@daum.net>
2023-05-09 13:43:20 +0900

    Feat : [Capstone Project] models.py 추가
    # tables
    - Member
    - Template
    - Template_type
    - Text
    - Introduce
    - Image

--- templates/models.py
+++ templates/models.py
@@ -1,26 +1,59 @@
 from django.db import models
 
 
-# Create your models here.
-class Question(models.Model):
-    subject = models.CharField(max_length=200)
-    content = models.TextField()
-    create_date = models.DateTimeField()
-
-    def __str__(self):
-        return self.subject + self.content
-
-
-class Answer(models.Model):
-    # Question 삭제시 Answer 삭제
-    # Question 1 : N Answer
-    question = models.ForeignKey(Question, on_delete=models.CASCADE)
-    content = models.TextField()
-    create_date = models.DateTimeField()
-
-
+# 회원 테이블
 class Member(models.Model):
     name = models.CharField(max_length=100)
     email = models.CharField(max_length=100)
     password = models.CharField(max_length=100)
     nick_name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name + ": [ 이메일 : " + self.email + " ]"
+
+
+# 템플릿 테이블 유형
+class Template_type(models.Model):
+    name = models.CharField(max_length=255)
+
+
+# 템플릿 테이블
+class Template(models.Model):
+    member = models.ForeignKey(Member, on_delete=models.CASCADE, db_column="member_id", null=False)
+    type = models.ForeignKey(Template_type, db_column="template_type_id")
+
+    def __str__(self):
+        return self.member.name + " 의 템플릿 입니다."
+
+
+# 이미지 테이블
+class Image(models.Model):
+    left = models.FloatField(default=0.0, blank=True)
+    top = models.FloatField(default=0.0)
+    height = models.FloatField(default=0.0)
+    width = models.FloatField(default=0.0)
+    curvature = models.FloatField(default=0.0)
+    transparency = models.FloatField(default=0.0)
+    angle = models.FloatField(default=0.0)
+    template = models.ForeignKey(Template, on_delete=models.CASCADE)
+
+
+# 소개 테이블
+class Introduce(models.Model):
+    src = models.CharField(max_length=255)
+    type = models.CharField(max_length=100)
+    src_type = models.CharField(max_length=20)
+    introduce = models.TextField
+
+
+# 텍스트 테이블
+class Text(models.Model):
+    text = models.TextField()
+    size = models.IntegerField(default=0)
+    pont = models.CharField(max_length=50)
+    left = models.FloatField(default=0.0, blank=True)
+    top = models.FloatField(default=0.0)
+    angle = models.FloatField(default=0.0)
+    text_color = models.CharField(max_length=50)
+    background_color = models.CharField(max_length=50)
+    transparency = models.FloatField(default=0.0)
