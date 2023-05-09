60a813f1b79adffae50312f8d8020210eb66a1f5
choidongkuen <danaver12@daum.net>
2023-05-09 17:35:14 +0900

    REFACTOR : [Capstone Project] models.py 추가 수정[2]

--- templates/models.py
+++ templates/models.py
@@ -4,7 +4,7 @@ from django.db import models
 # 회원 테이블
 class Member(models.Model):
     name = models.CharField(max_length=100)
-    email = models.CharField(max_length=100)
+    email = models.EmailField(max_length=100)
     password = models.CharField(max_length=100)
     nick_name = models.CharField(max_length=100)
 
@@ -21,8 +21,8 @@ class Template_type(models.Model):
 class Template(models.Model):
     member = models.ForeignKey(Member, on_delete=models.CASCADE, db_column="member_id", null=False)
     type = models.ForeignKey(Template_type, on_delete=models.CASCADE, db_column="template_type_id")
-    created_at = models.TimeField(auto_now_add=True)
-    update_at = models.TimeField(auto_now=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    update_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.member.name + " 의 템플릿 입니다."
@@ -30,13 +30,13 @@ class Template(models.Model):
 
 # 이미지 테이블
 class Image(models.Model):
-    left = models.FloatField(default=0.0, blank=True)
-    top = models.FloatField(default=0.0)
-    height = models.FloatField(default=0.0)
-    width = models.FloatField(default=0.0)
-    curvature = models.FloatField(default=0.0)
-    transparency = models.FloatField(default=0.0)
-    angle = models.FloatField(default=0.0)
+    left = models.FloatField()
+    top = models.FloatField()
+    height = models.FloatField()
+    width = models.FloatField()
+    curvature = models.FloatField()
+    transparency = models.FloatField()
+    angle = models.FloatField()
     template = models.ForeignKey(Template, on_delete=models.CASCADE)
 
 
@@ -52,12 +52,12 @@ class Introduce(models.Model):
 # 텍스트 테이블
 class Text(models.Model):
     text = models.TextField()
-    size = models.IntegerField(default=0)
+    size = models.IntegerField()
     pont = models.CharField(max_length=50)
-    left = models.FloatField(default=0.0, blank=True)
-    top = models.FloatField(default=0.0)
-    angle = models.FloatField(default=0.0)
+    left = models.FloatField()
+    top = models.FloatField()
+    angle = models.FloatField()
     text_color = models.CharField(max_length=50)
     background_color = models.CharField(max_length=50)
-    transparency = models.FloatField(default=0.0)
+    transparency = models.FloatField()
     template = models.ForeignKey(Template, on_delete=models.CASCADE)
