2ede0302754603cffae041ae09dba7bbae0fa26b
wjdals3936 <101387993+wjdals3936@users.noreply.github.com>
2023-05-12 00:00:10 +0900

    FEAT: 템플릿 이름 조회 기능 추가

--- templates/views.py
+++ templates/views.py
@@ -1,5 +1,6 @@
 from django.core import serializers
 from django.http import HttpResponse
+from templates.models import Template
 
 
 def index(request):
@@ -11,4 +12,17 @@ def index(request):
 
 
 def category_get(request, category_id):
-    return HttpResponse("%d 번의 카테고리[] 입니다." % category_id)
+    return HttpResponse("%d 번의 카테 고리[] 입니다." % category_id)
+
+
+def template_search(request):
+    tem_list = Template.objects.all()
+    name = request.GET.get('name', '')  # 검색어
+    if name:
+        tem_list = tem_list.filter(name__icontains=name)  # get 값을 가지는 필드의 내용을 가져 오기
+    json_template_list = serializers.serialize("json", tem_list)
+    return HttpResponse(json_template_list)
+
+
+def choose(request):
+    return HttpResponse("ㅎㅎ")
