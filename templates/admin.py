d1800d4c5517904093848df3a66a6ff8e98e0026
choidongkuen <danaver12@daum.net>
2023-05-13 02:17:29 +0900

    FEAT : Model Admin class 구현 및 적용
    - [적용한 모델]
    1. Template
    2. TemplateCategory
    3. TemplateTag
    4. Member

--- templates/admin.py
+++ templates/admin.py
@@ -4,10 +4,27 @@ from django.contrib import admin
 
 from templates.models import Template, Member, TemplateCategory, TemplateTag, Text, Image, Introduce
 
-admin.site.register(Template)
-admin.site.register(Member)
-admin.site.register(TemplateCategory)
-admin.site.register(TemplateTag)
+
+class MemberAdmin(admin.ModelAdmin):
+    search_fields = ['name']
+
+
+class TemplateAdmin(admin.ModelAdmin):
+    search_fields = ['name']
+
+
+class TemplateCategoryAdmin(admin.ModelAdmin):
+    search_fields = ['name']
+
+
+class TemplateTagAdmin(admin.ModelAdmin):
+    search_fields = ['tag_name']
+
+
+admin.site.register(Template, TemplateAdmin)
+admin.site.register(Member, MemberAdmin)
+admin.site.register(TemplateCategory, TemplateCategoryAdmin)
+admin.site.register(TemplateTag, TemplateTagAdmin)
 admin.site.register(Text)
 admin.site.register(Image)
 admin.site.register(Introduce)
