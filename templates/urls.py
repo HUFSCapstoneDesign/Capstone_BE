d8d917c49dbf3c305cfb9c39adbee1dcafa58556
wjdals3936 <101387993+wjdals3936@users.noreply.github.com>
2023-05-20 02:12:29 +0900

    FEAT: 템플릿 편집 api 구현

--- templates/urls.py
+++ templates/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path
-
 from . import views
 
 app_name = "templates"
 
 urlpatterns = [
     path("", views.index, name="index"),  # 템플릿 전체 보여 주기
-    path("<int:category_id>/", views.get_templates_by_category_id, name="category_get"),  # 카테 고리 선택 조회
+    path("<int:category_id>/", views.get_templates_by_category_id, name="category_get"),  # 카테고리 선택 조회
     path("search/", views.template_search, name="template_search"),  # 카테고리 이름 조회 # /templates/search/
-    path("<int:tem_id>/", views.template_choose, name="template_choose")  # 템플릿 선택
+    path("edit/", views.template_text_edit, name="text_edit"),  # 템플릿 편집창  # /templates/edit
+    # path("edit/<int:image_id>>", views.template_image_edit, name="image_edit")  # 템플릿 편집창  # /templates/edit
 ]
--- templates/views.py
+++ templates/views.py
@@ -1,12 +1,14 @@
+import json
 from django.core import serializers
 from django.http import HttpResponse
 from django.http import JsonResponse
 from django.shortcuts import get_object_or_404
-from templates.models import Template, TemplateCategory
+from templates.models import Template, TemplateCategory, Text, Image
 from django.views.decorators.csrf import csrf_exempt
+# from django.views.decorators.http import require_POST
 
 
-
+# 템플릿 선택창
 def index(request):
     from templates.models import Template
     template_list = Template.objects.all().order_by("-created_at")
@@ -26,12 +28,117 @@ def template_search(request):
     json_template_list = serializers.serialize("json", tem_list)
     return HttpResponse(json_template_list)
 
+# 템플릿 설명창
+
+
+# 템플릿 편집창
 @csrf_exempt
-def template_choose(request):
+def template_text_edit(request):
+    # if request.method == 'POST' or request.method == 'GET':
     if request.method == 'POST':
-        tem_id = request.POST.get('tem_id')
-        tem = Template.objects.filter(pk=tem_id)
-        json_template_list = serializers.serialize("json", tem)
-        return JsonResponse(json_template_list)
+        template_id = request.POST.get('template_id')
+        template_category_id = request.POST.get('category_id')
+        text_id = request.POST.get('text_id')
+        try:
+            template = Template.objects.get(id=template_id)
+            template_category = TemplateCategory.objects.get(id=template_category_id)
+            text = Text.objects.get(id=text_id)
+        except (Template.DoesNotExist, TemplateCategory.DoesNotExist):
+            return JsonResponse({'error': 'Template or Template Category not found'}, status=404)
+
+        data = [
+            {
+                'model': 'templates.template',
+                'pk': template.id,
+                'fields': {
+                    'name': template.name,
+                    'member': template.member.id,
+                    'template_category': template.template_category.id,
+                    'created_at': template.created_at,
+                    'update_at': template.update_at,
+                }
+            },
+            {
+                'model': 'templates.templatecategory',
+                'pk': template_category.id,
+                'fields': {
+                    'name': template_category.name,
+                }
+            },
+            {
+                'model': 'templates.text',
+                'pk': text.id,
+                'fields': {
+                    'content': text.content,
+                    'size': text.size,
+                    'pont': text.pont,
+                    'x': text.x,
+                    'y': text.y,
+                    'angle': text.angle,
+                    'text_color': text.text_color,
+                    'back_color': text.back_color,
+                    'cursive': text.cursive,
+                    'align': text.align,
+                    'line': text.line,
+                    'textopa': text.textopa,
+                    'backopa': text.backopa,
+                    'zindex': text.zindex,
+                    'template': text.template.id,
+                }
+            }
+        ]
+
+        response_data = json.dumps(data)
+        return JsonResponse(response_data, safe=False)
     else:
-        return JsonResponse({'error': 'Invalid request method.'})
+        return JsonResponse({'error': 'Method not allowed'}, status=405)
+
+# @csrf_exempt
+# def template_text_edit(request):
+#     if request.method == 'POST' or request.method == 'GET':
+#     # if request.method == 'POST':
+#         template_id = request.GET.get('template_id')
+#         template_category_id = request.GET.get('category_id')
+#         text_id = request.GET.get('text_id')
+#         try:
+#             template = Template.objects.get(id=template_id)
+#             template_category = TemplateCategory.objects.get(id=template_category_id)
+#             text = Text.objects.get(id=text_id)
+#         except (Template.DoesNotExist, TemplateCategory.DoesNotExist):
+#             return JsonResponse({'error': 'Template or Template Category not found'}, status=404)
+#
+#         response_data = {
+#             'template_id': template.id,
+#             'template_name': template.name,
+#             'template_category_id': template_category.id,
+#             'template_category_name': template_category.name,
+#             'x': text.x,
+#             'y': text.y
+#         }
+#
+#         return JsonResponse(response_data)
+#     else:
+#         return JsonResponse({'error': 'Method not allowed'}, status=405)
+
+
+@csrf_exempt
+def template_image_edit(request):
+    if request.method == 'POST':
+        template_id = request.GET.get('template_id')
+        template_category_id = request.GET.get('category_id')
+        image_id = request.GET.get('image_id')
+        try:
+            template = Template.objects.get(id=template_id)
+            template_category = TemplateCategory.objects.get(id=template_category_id)
+            image = Image.objects.get(id=image_id)
+        except (Template.DoesNotExist, TemplateCategory.DoesNotExist):
+            return JsonResponse({'error': 'Template or Template Category not found'}, status=404)
+
+        response_data = dict(template_id=template.id, template_name=template.name,
+                             template_category_id=template_category.id, template_category_name=template_category.name,
+                             x=Image.x, y=Image.y, height=Image.height, width=Image.width, cursive=Image.cursive,
+                             transparency=Image.transparency, angle=Image.angle, template=Image.template)
+
+        return JsonResponse(response_data)
+    else:
+        return JsonResponse({'error': 'Method not allowed'}, status=405)
