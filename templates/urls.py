7a4bec8b3e026b06a7af09a594526033d4530335
choidongkuen <danaver12@daum.net>
2023-05-22 04:32:13 +0900

    REFACTOR : views 관련 리팩토링

--- templates/urls.py
+++ templates/urls.py
@@ -6,8 +6,9 @@ app_name = "templates"
 
 urlpatterns = [
     path("", views.index, name="index"),  # 템플릿 전체 보여 주기
-    path("<int:category_id>/", views.get_templates_by_category_id, name="category_get"),  # 카테 고리 선택 조회
+    path("<int:category_id>/", views.get_templates_by_category_id, name="category_get"),  # 카테고리 선택 조회
     path("search/", views.template_search, name="template_search"),  # 카테고리 이름 조회 # /templates/search/
-    path("<int:template_id>/explain/", views.show_template_explain, name="template_explain"),  # 작성한 템플릿 설명창(팝업창)
-    # path("<int:template_id>/", views.template_choose, name="template_choose")  # 템플릿 선택
+    path("<int:template_id>/explain", views.show_template_explain, name="template_show"),  # 템플릿 설명(팝업창)
+    path("edit/", views.template_text_edit, name="text_edit"),  # 텍스트 편집  # /templates/edit
+    # path("edit/", views.template_image_edit, name="image_edit"),  # 이미지 편집  # /templates/edit
 ]
